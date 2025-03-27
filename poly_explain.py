37be58425cf2a432017e02749f8e48224711c631
ElyorGhost <elyor.ghost@gmail.com>
2025-03-27 16:08:45 +0500

    OOP is finished

---
+++ poly_explain.py
@@ -0,0 +1,8 @@
+name = "Elyor"
+print(len(name))
+
+some_list = ["some", "name"] # list
+print(len(some_list))
+
+# That's polymorphism in action, a single fuction does now
+# how to handle different kind of objects as expected!
